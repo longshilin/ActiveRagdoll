8016eccfde446a82187082b99ea8dfbe91343043
longshilin <583297550@qq.com>
2022-11-17 01:07:16 +0800

    update

---
+++ Assets/RaycastShooter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+	public class RaycastShooter : MonoBehaviour {
+
+		public LayerMask layers;
+		public float force = 10000f;
+		public ParticleSystem blood;
+
+        // Update is called once per frame
+        void Update () {
+			if (Input.GetMouseButtonDown(0)) {
+				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+				// Raycast to find a ragdoll collider
+				RaycastHit hit = new RaycastHit();
+				if (Physics.Raycast(ray, out hit, 100f, layers)) {
+					var broadcaster = hit.collider.attachedRigidbody;
+
+					if (broadcaster != null) {
+						broadcaster.AddForce(ray.direction * force);
+                        
+
+						// blood.transform.position = hit.point;
+						// blood.transform.rotation = Quaternion.LookRotation(-ray.direction);
+						// blood.Emit(5);
+					}
+				}
+			}
+		
+	}
+}
